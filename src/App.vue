5d10cd0653f3c7046fc770103498b8ca082255fd
BlackCoder56 <blackcoda56@gmail.com>
2025-07-29 00:49:02 +0300

    Setting up the App.vue with sections

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,33 @@
 </script>
 
 <template>
-  <header>
-
-  </header>
-
-  <main>
-    
+  <main class="relative">
+    Nav
+    <section class="sm:px-16 px-8 sm:py-24 py-12">
+      Hero
+    </section>
+ 
+    <section class="sm:px-16 px-8 sm:py-24 py-12">
+      PopularProducts
+    </section>
+    <section class="sm:px-16 px-8 sm:py-24 py-12">
+      SuperQuality
+    </section>
+    <section class="sm:px-16 px-8 py-10">
+      Services
+    </section>
+    <section class="sm:px-16 px-8 sm:py-24 py-12">
+      SpecialOffer
+    </section> 
+    <section  class="bg-blue-100 sm:px-16 px-8 sm:py-24 py-12">
+      CustomerReviews
+    </section> 
+    <section class="sm:px-16 px-8 sm:py-32 py-16 w-full">
+      Subscribe
+    </section> 
+    <section class="bg-black sm:px-16 px-8 sm:pt-24 pt-12 pb-8">
+      Footer
+    </section> 
   </main>
 </template>
 
