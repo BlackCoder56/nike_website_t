dec0db210acdf9715042cb260a64307b60616a5b
BlackCoder56 <blackcoda56@gmail.com>
2025-07-30 21:46:25 +0300

    Created section folder with section vue files

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,45 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
+import {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality
+} from './sections'
 </script>
 
 <template>
   <main class="relative">
-    Nav
+    <!-- <Nav/> -->
     <section class="sm:px-16 px-8 sm:py-24 py-12">
-      Hero
+      <Hero/>
     </section>
  
     <section class="sm:px-16 px-8 sm:py-24 py-12">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section class="sm:px-16 px-8 sm:py-24 py-12">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section class="sm:px-16 px-8 py-10">
-      Services
+      <Services/>
     </section>
     <section class="sm:px-16 px-8 sm:py-24 py-12">
-      SpecialOffer
+      <SpecialOffer/>
     </section> 
     <section  class="bg-blue-100 sm:px-16 px-8 sm:py-24 py-12">
-      CustomerReviews
+      <CustomerReviews/>
     </section> 
     <section class="sm:px-16 px-8 sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section> 
     <section class="bg-black sm:px-16 px-8 sm:pt-24 pt-12 pb-8">
-      Footer
+      <Footer/>
     </section> 
   </main>
 </template>
