85f72cc5774f8f7cbadc72c947c14eb072fdbc57
BlackCoder56 <blackcoda56@gmail.com>
2025-08-02 09:47:22 +0300

    Creating navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,3 +1,15 @@
+<script>
+    // import { headerLogo } from '@/assets/images';
+    import headerLogo from '@/assets/images/customer1.jpeg';
+    // import { hamburger } from '@/assets/icons';
+</script>
+
 <template>
-    <h1>text</h1>
+    <header class="padding-x py-8 absolute z-10 w-full">
+        <nav>
+            <a href="/">
+                <img :src='headerLogo' alt="Logo">
+            </a>
+        </nav>
+    </header>
 </template>
