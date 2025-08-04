fe88ab266362da2f9331e6d92eaeb55136007a3e
BlackCoder56 <blackcoda56@gmail.com>
2025-08-04 16:50:24 +0300

    Added width and height to Logo and imported hamburger icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,20 @@
 <script setup>
 import headerLogo from '@/assets/images/headerlogo.svg?url';
+import { hamburger } from '@/assets/icons';
 </script>
 
 <template>
   <header class="padding-x py-8 absolute z-10 w-full">
     <nav>
       <a href="/">
-        <img :src="headerLogo" alt="Logo" class="w-32 h-auto" />
+        <img 
+        :src="headerLogo" 
+        alt="Logo" 
+        :style="
+        {
+          width:'130px',
+          height:'29px'
+        }" />
       </a>
     </nav>
   </header>
