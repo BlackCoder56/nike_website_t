a981cfd3519721d59fc7cf058186dcbe48c7d7c9
BlackCoder56 <blackcoda56@gmail.com>
2025-08-08 12:41:35 +0300

    Created navlinks

---
+++ src/assets/images/index.js
@@ -0,0 +1,57 @@
+// Importing big shoes' images
+import bigShoe1 from './big-shoe1.png'
+import bigShoe2 from './big-shoe2.png'
+import bigShoe3 from './big-shoe3.png'
+
+// Importing customer images
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+// Importing footer and header logo
+import footerLogo from './footer-logo.svg'
+import headerLogo from './header-logo.svg';
+
+// Importing offer banner
+import offer from './offer.svg'
+
+// Importing more shoes
+import shoe4 from './shoe4.svg'
+import shoe5 from './shoe5.svg'
+import shoe6 from './shoe6.svg'
+import shoe7 from './shoe7.svg'
+import shoe8 from './shoe8.svg'
+
+// Importing thumbnails
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailShoe1 from './thumbnail-shoe1.svg'
+import thumbnailShoe2 from './thumbnail-shoe2.svg'
+import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
+export {
+    // Exporting Big Shoes
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+
+    // Customers
+    customer1,
+    customer2,
+
+    // Exporting Logos
+    footerLogo,
+    headerLogo,
+
+    // Exporting More shoes
+    offer,
+    shoe4,
+    shoe5,
+    shoe6,
+    shoe7,
+    shoe8,
+
+    // Exporting Thumbnails
+    thumbnailBackground,
+    thumbnailShoe1,
+    thumbnailShoe2,
+    thumbnailShoe3
+}
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <script setup>
-import headerLogo from '@/assets/images/headerlogo.svg?url';
-import { hamburger } from '@/assets/icons';
+import { headerLogo } from '@/assets/images'; // No ?url
+import { navLinks } from '@/constants' // imports array of nav links
 </script>
 
 <template>
@@ -8,17 +8,23 @@ import { hamburger } from '@/assets/icons';
     <nav class="flex justify-between items-center max-w-[1440px] mx-auto">
       <a href="/">
         <img 
-        :src="headerLogo" 
-        alt="Logo" 
-        :style="
-        {
-          width:'130px',
-          height:'29px'
-        }" />
-        <ul class="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          
-        </ul>
+          :src="headerLogo" 
+          alt="Logo" 
+          :style="{ width: '130px', height: '29px' }"
+        />
       </a>
+
+      <!-- nav links (desktop) -->
+      <ul class="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <li v-for="item in navLinks" :key="item.label">
+          <a 
+            :href="item.href"
+            class="font-montserrat leading-normal text-lg text-slate-500"
+          >
+            {{ item.label }}
+          </a>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
